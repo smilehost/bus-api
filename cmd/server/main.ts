5ff73cc703b00a07ac50ce9186f0ae8997e5df35
SuphasanManitsaKKU <suphasan.m@kkumail.com>
2025-04-21 20:21:44 +0700

    feat: connect databse ok

--- cmd/server/main.ts
+++ cmd/server/main.ts
@@ -1,15 +1,17 @@
 import express from 'express';
-
+import { PrismaClient } from '@prisma/client';
 
 const app = express();
 const port = 3000;
 
+const prisma = new PrismaClient();
+
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/', async (req, res) => {
+  const priceTypes = await prisma.price_type.findMany();
+    res.json(priceTypes);
 });
-
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
--- internal/model/item.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type Item struct {
-	ID    string `json:"id"`
-	Name  string `json:"name"`
-	Price string `json:"price"`
-	Stock string `json:"stock"`
-}
