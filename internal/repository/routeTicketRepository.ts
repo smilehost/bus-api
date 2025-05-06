c408ed0251329b7ccc8630be4a800beb5d3fb653
=Aekarattakhat <=aekarattakhat.s@kkumail.com>
2025-05-06 17:22:33 +0700

    add create and del priceType(complete)

--- internal/repository/routeTicketRepository.ts
+++ internal/repository/routeTicketRepository.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import { RouteTicketWithPrices } from "../../cmd/request";
+import { RouteTicketPriceType } from "../../cmd/models";
 import { AppError } from "../utils/appError";
 
 export class RouteTicketRepository {
@@ -215,6 +216,32 @@ export class RouteTicketRepository {
         return await tx.route_ticket.delete({
           where: { route_ticket_id: id },
         });
+      });
+    } catch (error) {
+      throw AppError.fromPrismaError(error);
+    }
+  }
+
+  async createPriceType(comId: number, data: RouteTicketPriceType) {
+    try {
+      return await this.prisma.route_ticket_price_type.create({
+        data: {
+          route_ticket_price_type_name: data.route_ticket_price_type_name,
+          route_ticket_price_type_com_id: comId,
+        },
+      });
+    } catch (error) {
+      throw AppError.fromPrismaError(error);
+    }
+  }
+
+  async deletePriceType(comId: number, priceTypeId: number) {
+    try {
+      return await this.prisma.route_ticket_price_type.deleteMany({
+        where: {
+          route_ticket_price_type_id: priceTypeId,
+          route_ticket_price_type_com_id: comId,
+        },
       });
     } catch (error) {
       throw AppError.fromPrismaError(error);
