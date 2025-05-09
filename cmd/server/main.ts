c321ce1370457a53b438375c917cdbf2f7adee8a
SuphasanManitsaKKU <suphasan.m@kkumail.com>
2025-05-09 11:52:50 +0700

    update!!

--- cmd/server/main.ts
+++ cmd/server/main.ts
@@ -18,7 +18,7 @@ app.use(
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello Secure World!!!!!!2");
+  res.send("Hello Secure World!!!!!!3");
 });
 
 app.get("/areYouPay", (req, res) => {
