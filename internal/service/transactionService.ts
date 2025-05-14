1bdaac562c152dda5706d7066398e450e623d6cc
SuphasanManitsaKKU <suphasan.m@kkumail.com>
2025-05-14 14:52:06 +0700

    Comment out transaction controller and service code for future refactoring

--- internal/service/transactionService.ts
+++ internal/service/transactionService.ts
@@ -1,19 +1,19 @@
-import { TransactionRepository } from "../repository/transactionRepository";
-import { CompanyRepository } from "../repository/companyRepository";
-import { CreateTransactionDto } from "../../cmd/dto";
-import { AppError } from "../utils/appError";
+// import { TransactionRepository } from "../repository/transactionRepository";
+// import { CompanyRepository } from "../repository/companyRepository";
+// import { CreateTransactionDto } from "../../cmd/dto";
+// import { AppError } from "../utils/appError";
 
-export class TransactionService {
-  constructor(
-    private readonly transactionRepository: TransactionRepository,
-    private readonly companyRepository: CompanyRepository
-  ) {}
+// export class TransactionService {
+//   constructor(
+//     private readonly transactionRepository: TransactionRepository,
+//     private readonly companyRepository: CompanyRepository
+//   ) {}
 
-  async create(com_id: number, payload: CreateTransactionDto) {
+//   async create(com_id: number, payload: CreateTransactionDto) {
 
-    const company = await this.companyRepository.getById(com_id);
-    if (!company) throw AppError.NotFound("Company not found");
+//     const company = await this.companyRepository.getById(com_id);
+//     if (!company) throw AppError.NotFound("Company not found");
     
-    return this.transactionRepository.createAllInOneTransaction(com_id, payload);
-  }
-}
+//     return this.transactionRepository.createAllInOneTransaction(com_id, payload);
+//   }
+// }
