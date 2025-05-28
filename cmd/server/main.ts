aff93dcc2c5b9652117f5fd4d30f89d6e72fc7d6
SuphasanManitsaKKU <suphasan.m@kkumail.com>
2025-05-28 19:19:28 +0700

    ddd

--- cmd/server/main.ts
+++ cmd/server/main.ts
@@ -10,7 +10,6 @@ const prisma = new PrismaClient();
 
 app.disable("x-powered-by");
 
-
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(
@@ -67,7 +66,7 @@ app.get("/qr", (_, res) => {
         </style>
       </head>
       <body>
-        <img src="/qr.jpg" alt="QR Code" />
+        <img src="/api/qr.jpg" alt="QR Code" />
       </body>
     </html>
   `);
